9fc29e435b88071cd9d1beeba1cd71ef9938a0f3
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2012-03-28 09:38:31 -0700

    Moved all old unchecked code to oldsrc, added Maven buildfile and skeleton for plugin

---
+++ src/main/java/net/zhuoweizhang/moresphereworld/MoreSphereWorldPlugin.java
@@ -0,0 +1,16 @@
+package net.zhuoweizhang.moresphereworld;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MoreSphereWorldPlugin extends JavaPlugin implements Listener {
+    public void onDisable() {
+    }
+
+    public void onEnable() {
+    }
+
+}
+
